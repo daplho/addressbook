8698de444273a68327ed0adc18e3b31112fd96a1
niko <nkirov@ea.com>
2018-03-15 17:01:13 -0700

    reogranizing project

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"os"
 
 	pb "github.com/daplho/addressbook/tutorial"
 	"github.com/gogo/protobuf/proto"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s ADDRESS_BOOK_FILE\n", os.Args[0])
+	}
+	fname := os.Args[1]
+
 	book := &pb.AddressBook{}
 
 	// Write the new address book back to disk.
